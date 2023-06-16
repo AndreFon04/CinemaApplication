1acb0d601722f279bed4268369d25147c5902501
Gonçalo Costa <135621679+Barraca02@users.noreply.github.com>
2023-06-16 20:30:51 +0100

    Update PaymentDetails.cpp

--- sources/model/PaymentDetails.cpp
+++ sources/model/PaymentDetails.cpp
@@ -1,3 +1,75 @@
-//
-// Created by andre on 10-06-2023.
-//
+#include <iostream>
+#include <array>
+#include "PaymentInfo.h"
+
+    void PaymentInfo::getCardNumber() {
+        payment_info& info = userPInfo;
+
+            cout << "Please introduce your card number: " << endl;
+            cin >> info.card_number;
+
+    }
+
+    void PaymentInfo::getCVV() {
+        payment_info& info = userPInfo;
+
+            cout << "Please introduce your CVV number: " << endl;
+            cin >> info.cvv;
+
+    }
+
+    void PaymentInfo::getCardName() {
+        payment_info& info = userPInfo;
+
+            cout << "Please introduce the card owner's name (please type a '_' in between the names): " << endl;
+            cin >> info.owners_name;
+
+    }
+
+    void PaymentInfo::AccountgetP_Info() {
+
+        payment_info& info = userPInfo;
+        char answer = 'Y';
+        int answer2;
+
+        while(answer == 'Y' || answer == 'y') {
+            if(answer == 'Y' || answer == 'Y') {
+
+                getCardNumber();
+                getCVV();
+                getCardName();
+
+                cout << "-------/-------\nCard Number: " << info.card_number << "\nCVV number: " << info.cvv
+                     << "\nOwner's name: " << info.owners_name << "\n-------/-------\nWould you like to change your info?\nY/N: "
+                     << endl;
+                cin >> answer;
+            }
+
+            if(answer == 'Y' || answer == 'y') {
+                cout << "Which part would you like to change:\n[1] - Card number\n[2] - CVV number\n[3] - Card Name\n[4] - All of them\n" << endl;
+                cin >> answer2;
+
+                switch (answer2) {
+                    case 1:
+                        getCardNumber();
+                        answer = 'N';
+                        break;
+
+                    case 2:
+                        getCVV();
+                        answer = 'N';
+                        break;
+
+                    case 3:
+                        getCardName();
+                        answer = 'N';
+                        break;
+
+                    case 4:
+                        break;
+                }
+            }
+            cout << "-------/-------\nCard Number: " << info.card_number << "\nCVV number: " << info.cvv << "\nOwner's name: " << info.owners_name << "\n-------/-------" << endl;
+        }
+
+    }
